f0c2a19415ae7ea656875f344ce914e67954cc85
Tatyana305-png <tanchik07k@gmail.com>
2026-03-02 22:24:48 +0300

    Update CompilerState.py

--- lab_1/CompilerModel/Compiler/Models/CompilerState.py
+++ lab_1/CompilerModel/Compiler/Models/CompilerState.py
@@ -10,3 +10,22 @@ class CompilerState(Enum):
     CODE_GENERATION = "Генерация кода"
     COMPLETED = "Завершено"
     ERROR = "Ошибка"
+
+    @classmethod
+    def get_all_stages(cls):
+        """Возвращает все этапы компиляции по порядку"""
+        return [
+            cls.LEXICAL_ANALYSIS,
+            cls.SYNTAX_ANALYSIS,
+            cls.OPTIMIZATION,
+            cls.CODE_GENERATION,
+            cls.COMPLETED
+        ]
+
+    @classmethod
+    def get_stage_index(cls, stage):
+        """Возвращает индекс этапа"""
+        stages = cls.get_all_stages()
+        if stage in stages:
+            return stages.index(stage)
+        return -1
